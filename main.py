422f0fca1bb70580fc2c4fecde505367461776c6
Uralcan Sekerci <85555680+UralcanSekerci@users.noreply.github.com>
2022-12-05 18:57:03 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,46 @@
+
+def madlib1():
+    animals = input('Enter an animal name: ')
+    profession = input('Enter a profession: ')
+    cloth = input('Enter a piece of cloth name: ')
+    things = input('Enter a thing: ')
+    name = input('Enter a name: ')
+    place = input('Enter a place name: ')
+    verb = input('Enter a verb in -ing form: ')
+    food = input('Food name: ')
+    story = f'say {food} the photographer said as the camera flashed! {name} and I had gone to {place} to get our photos taken on my birthday. The first photo we really wanted was a picture of us dressed as {animals} pretending to be a {profession}. when we saw the second photo, it was exactly what I wanted. We both looked like {things} wearing {cloth} and  {verb}--exactly what I had in mind'
+    print(story)
+
+madlib1()
+
+def madlib2():
+    adjective = input('enter an Adjective : ')
+    color = input('Enter a color: ')
+    thing = input('Enter a thing:')
+    place = input('Enter a place name: ')
+    person = input('Enter a person name: ')
+    adjective1 = input('Enter an adjective: ')
+    insect = input('Enter an insect name: ')
+    food = input('Enter a food name: ')
+    verb = input('Enter a verb name: ')
+    story = f'Last night I dreamed I was a {adjective} butterfly with {color} splotches that looked like {thing} .I flew to {place} with my best friend and {person} who was a {adjective1} {insect} .We ate some {food} when we got there and then decided to {verb} and the dream ended when I said-- lets {verb}.'
+    print(story)
+
+madlib2()
+
+def madlib3():
+    person = input('Enter a person name: ')
+    color = input('Enter a color : ')
+    foods = input('Enter a food name : ')
+    adjective = input('Enter an adjective: ')
+    thing = input('Enter a thing: ')
+    place = input('Enter a place: ')
+    verb = input('Enter a verb: ')
+    adverb = input('Enter an adverb: ')
+    food = input('Enter food name: ')
+    things = input('Enter a thing: ')
+    story = f'Today we picked apple from {person}\'s Orchard. I had no idea there were so many different varieties of apples. I ate {color} apples straight off the tree that tested like {foods}. Then there was a {adjective} apple that looked like a {thing}.When our bag were full, we went on a free hay ride to {place} and back. It ended at a hay pile where we got to {verb} {adverb} I can hardly wait to get home and cook with the apples. We are going to make apple {food} and {things} pies!.'
+    print(story)
+
+madlib3()
+
